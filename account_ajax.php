a29ca9ed7e00763cc9045e075ee7812432655bed
shivani <gituser>
2022-10-15 13:28:01 +0530

    new github folder with 2 files

---
+++ account_ajax.php
@@ -0,0 +1,10 @@
+<?php include 'inc_classes.php';
+
+	$bank_id=$_REQUEST['bank_id'];
+	$sel_bank = "select account_no from bank where bank_id='".$bank_id."' ";
+	$execute_bank = mysql_query($sel_bank);
+	$account_no= mysql_fetch_array($execute_bank);
+	echo $account_no['account_no'];
+
+
+?>
